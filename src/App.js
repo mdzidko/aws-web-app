6d5280e559f1b8da2d4373f177d30630ec464198
Michał Dzidko <mdzidko@novomatic-tech.com>
2021-08-11 15:29:00 +0200

    button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">Primary</Button>
     </div>
   );
 }
